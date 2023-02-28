6d2851ccb8d980f8b21c3a9912935786d3a9ac6a
HongEuiPyo <ant1993j@gmail.com>
2023-02-28 13:02:54 +0900

    reviewDay12

---
+++ src/reviewDay12/Study_condition.java
@@ -0,0 +1,48 @@
+package reviewDay12;
+
+public class Study_condition {
+    public static void main(String[] args) {
+
+        // 1. if문
+        int i = 10;
+
+        if (i<5) {
+            System.out.println("참");
+        } else if (i<3) {
+            System.out.println("거짓");
+        } else {
+            System.out.println("나머지");
+        }
+
+        // 2. 삼항 연산자 조건문
+        boolean isMarried = true;
+        boolean isMan = true;
+
+        String str;
+        str = isMarried ? "결혼 했다" : "결혼 안 했다";
+        System.out.println(str);
+
+        // 3. 논리 연산
+        if (isMarried && isMan) {
+            str = "결혼 했다";
+        } else {
+            str = "결혼 안 했다";
+        }
+
+        System.out.println(str);
+
+        // 4. switch문
+        switch (str) {
+            case "결혼 했다":
+                System.out.println("O");
+                break;
+            case "결혼 안 했다":
+                System.out.println("X");
+                break;
+            default:
+                System.out.println("?");
+                break;
+        }
+
+    }
+}
---
+++ src/reviewDay12/models/Character.java
@@ -0,0 +1,7 @@
+package reviewDay12.models;
+
+public abstract class Character extends Person {
+
+    public abstract void attack(Hero hero);
+
+}
